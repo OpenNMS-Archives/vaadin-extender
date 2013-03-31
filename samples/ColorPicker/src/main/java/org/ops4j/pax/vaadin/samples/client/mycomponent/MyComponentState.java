b7714674ac2af46632ebd48bb6886ccf859ab2ed
Donald Desloge <thedesloge@gmail.com>
2013-03-31 14:25:08 -0400

    files removed

--- samples/ColorPicker/src/main/java/org/ops4j/pax/vaadin/samples/client/mycomponent/MyComponentState.java
+++
@@ -1,9 +0,0 @@
-package org.ops4j.pax.vaadin.samples.client.mycomponent;
-
-import com.vaadin.shared.AbstractComponentState;
-
-public class MyComponentState extends AbstractComponentState {
-
-    // TODO example state
-    public String text = "This is MyComponent";
-}
