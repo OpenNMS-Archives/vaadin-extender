4dff8e3ef1d7676183e9fdbee14c994d002f3d29
Benjamin Reed <ranger@opennms.org>
2014-10-28 17:38:58 -0400

    remove samples

--- samples/ColorPicker/src/main/java/org/ops4j/pax/vaadin/samples/internal/client/mycomponent/MyComponentState.java
+++
@@ -1,9 +0,0 @@
-package org.ops4j.pax.vaadin.samples.internal.client.mycomponent;
-
-import com.vaadin.shared.AbstractComponentState;
-
-public class MyComponentState extends AbstractComponentState {
-
-    // TODO example state
-    public String text = "This is MyComponent";
-}
